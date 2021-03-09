f8255ab93435256f475827c5128b0f701d7c5229
Codinator29 <67217624+Codinator29@users.noreply.github.com>
2021-03-09 22:21:40 +0530

    Add files via upload

---
+++ Powx-n.cpp
@@ -0,0 +1,20 @@
+/*
+
+https://leetcode.com/problems/powx-n/
+
+Implement pow(x, n), which calculates x raised to the power n (i.e. xn).
+
+*/
+
+class Solution {
+public:
+    double myPow(double x, long long n) {
+        if(n==0) 
+            return 1;
+        if(n<0) 
+            return 1/(myPow(x,-1*n));
+        if(n%2==1) 
+            return x*myPow(x,n-1);
+        return myPow(x*x,n/2);
+    }
+};
